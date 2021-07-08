869c373917c6df49480f979f99fdc30081489d9c
arjun <arjun@method-hub.com>
2021-07-08 10:31:38 +0530

    update build

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
-// export const baseUrl  = "http://3.12.65.48:4004"
-export const baseUrl  = "http://localhost:4004"
+export const baseUrl  = "http://3.12.65.48:4004"
+// export const baseUrl  = "http://localhost:4004"
 // export const img_url = "http://localhost:4004/"
 export const img_url = "http://3.12.65.48:4004/"
 export const CONSTANT_NUMBER_2 = 'hello I am also a constant';
